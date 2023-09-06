31c80c6b5c188c13a3300581b32c21b872f488e5
Flavio Ruvalcaba Leija <71139354+FlavioRr@users.noreply.github.com>
2023-09-06 13:08:35 -0600

    Regresion logistica con framework finalizada

--- MachineLearning1/ImplementacionConFramework.py
+++ MachineLearning1/ImplementacionConFramework.py
@@ -10,13 +10,46 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 
-# Paso 1: Cargar los datos
-#En mi variable data_path  anexo el path donde se encuentran los datos que quiero utilizar.
-data_path = "inputData/gpa_study_hours.csv"
+
+
+# Iniciamos a entrenar el modelo 
+# Cargamos los datos
+# En mi variable data_path  anexo el path donde se encuentran los datos que quiero utilizar.
+data_path_train = "inputData/gpa_study_hours_train.csv"
+data_path_test = "inputData/gpa_study_hours_test.csv"
 #Con la libreria pandas, utilizo el metodo read_csv para poder acceder al documento csv.
-data = pd.read_csv(data_path)
-# En la variable x e y identifico las columnas de la tabla de datos que quiero buscar su relacion.
-y = data['study_hours']
-x = data['gpa']
+data_train = pd.read_csv(data_path_train)
+data_test = pd.read_csv(data_path_test)
+#Creo dataframes para manipular las columnas del documento csv.
+df_train = pd.DataFrame(data_train, columns=["gpa", "study_hours", "graduated"])
+df_test = pd.DataFrame(data_test, columns=["gpa", "study_hours", "graduated"])
+
+# En la variable x identifico los valores de la tabla de datos que se obtienen de una observacion. En
+# este caso son gpa y study_hours.
+# En la variable y identifico los valores de la tabla de datos que existen como clasificacion binaria 
+# si un estudiante aprobo o reprobo. En este caso es la columna graduated
+
+x_train = df_train[["gpa", "study_hours"]]
+y_train = df_train["graduated"]
+x_test = df_test[["gpa", "study_hours"]]
+y_test = df_test["graduated"]
+
+
+
+
+
+#Definimos la cantidad de iteraciones que se utilizaran en el modelo.
+
+#Utilizamos la libreria sklearn utilizando regrsion logistica.
+model = LogisticRegression(penalty='none', max_iter=1000)
+model.fit(x_train, y_train)
+
+#Empezamos a crear predicciones con nuestros datos de test.
+y_pred = model.predict(x_test)
+print(y_pred)
+#Calculamos la precision de la prediccion con accuracy_score.
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Precisión del modelo: {accuracy * 100:.2f}%')
